82be5cbccf9e27bf20c85a7928562c2565bb4be9
CallumJBenvin <callum-benvin@outlook.com>
2020-01-20 22:04:37 +1300

    update

--- PartD/src/main.java
+++ PartD/src/main.java
@@ -2,7 +2,7 @@
 public class main {
 
 	public main(String[] args) {
-		System.out.println("Hi, my name is Penny");
+		System.out.println("Hi, my name is Callum");
 		System.out.println("My favourite colour is blue");
 		System.out.println("My favourite mode of transportation is by car");
 		System.out.println("The best day of the week is a Sunday");
