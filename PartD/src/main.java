9419d27106225d179b2e8f1371548e3120c0856f
CallumJBenvin <callum-benvin@outlook.com>
2020-01-20 22:02:31 +1300

    First upload

---
+++ PartD/src/main.java
@@ -0,0 +1,12 @@
+
+public class main {
+
+	public main(String[] args) {
+		System.out.println("Hi, my name is Callum");
+		System.out.println("My favourite colour is blue");
+		System.out.println("My favourite mode of transportation is by car");
+		System.out.println("The best day of the week is a Sunday");
+
+	}
+
+}
